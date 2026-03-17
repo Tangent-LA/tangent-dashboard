d071f2ea577345d27f51906be725fb26a3d347c5
Anshu <anshu.jalal08@gmail.com>
2026-03-17 11:08:10 +0400

    Delete layout.tsx

--- layout.tsx
+++
@@ -1,5 +0,0 @@
-import DashboardLayoutClient from './DashboardLayoutClient';
-
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return <DashboardLayoutClient>{children}</DashboardLayoutClient>;
-}
